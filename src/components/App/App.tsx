93c5d7156397674ecb895f5d036179e35a3278ce
Aїda <izograf.zinaida@gmail.com>
2026-01-13 01:20:04 +0200

    just save

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,25 +3,40 @@ import fetchMovies from '../../services/movieService';
 import type { Movie } from '../../types/movie';
 import SearchBar from '../SearchBar/SearchBar';
 import './App.module.css';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import { useState } from 'react';
 
 const notify = () => toast.error('No movies found for your request.');
 
 export default function App() {
+  const [isSuccess, setIsSuccess] = useState(true);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
   async function handleForm(query: string) {
     try {
-      const movies: Movie[] = await fetchMovies(query);
-      if (movies.length === 0) {
+      const result: Movie[] = await fetchMovies(query);
+      setMovies(result);
+      setIsSuccess(true);
+      if (result.length === 0) {
         notify();
       }
-      console.log(movies);
     } catch (err) {
+      setIsSuccess(false);
+      setMovies([]);
       console.error(err);
     }
   }
+  function handleSelect() {}
   return (
     <>
       <SearchBar onSubmit={handleForm} />
       <Toaster />
+      {isSuccess ? (
+        <MovieGrid onSelect={handleSelect} movies={movies} />
+      ) : (
+        <ErrorMessage />
+      )}
     </>
   );
 }
